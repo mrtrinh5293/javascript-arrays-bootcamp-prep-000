dc85a109034dccb0e3406ce8aaebf3869d7d88e6
Duc Dang <mrtrinh5293@gmail.com>
2018-04-26 04:49:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,46 +1,46 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(array, element){
-  return [element, ...array];
-}
+// function addElementToBeginningOfArray(array, element){
+//   return [element, ...array];
+// }
 
-function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element);
-  return array
-}
+// function destructivelyAddElementToBeginningOfArray(array, element){
+//   array.unshift(element);
+//   return array
+// }
 
-function addElementToEndOfArray(array, element){
-  // array.push(element);
-  // return array
+// function addElementToEndOfArray(array, element){
+//   // array.push(element);
+//   // return array
   
-  return [...array, element];
-}
+//   return [...array, element];
+// }
 
-function destructivelyAddElementToEndOfArray(array, element){
-  array.push(element);
-}
+// function destructivelyAddElementToEndOfArray(array, element){
+//   array.push(element);
+// }
 
-function accessElementInArray(array, index){
+// function accessElementInArray(array, index){
   
-  return array[index]
-}
+//   return array[index]
+// }
 
-function destructivelyRemoveElementFromBeginningOfArray(array){
-  array.slice(1);
-}
+// function destructivelyRemoveElementFromBeginningOfArray(array){
+//   array.slice(1);
+// }
 
-function removeElementFromBeginningOfArray(array){
-  array.shift();
-  return array
-}
+// function removeElementFromBeginningOfArray(array){
+//   array.shift();
+//   return array
+// }
 
-function destructivelyRemoveElementFromEndOfArray(array){
-  array.pop();
-  return array
-}
+// function destructivelyRemoveElementFromEndOfArray(array){
+//   array.pop();
+//   return array
+// }
 
-function removeElementFromEndOfArray(array){
-  array.slice(array.lenght-1)
-  return array
-}
+// function removeElementFromEndOfArray(array){
+//   array.slice(array.lenght-1)
+//   return array
+// }
 
