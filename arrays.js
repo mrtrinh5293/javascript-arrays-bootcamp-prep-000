e2c9dd32c2f8df8b2571b759d2af03975a3eb8b9
Duc Dang <mrtrinh5293@gmail.com>
2018-04-26 04:18:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,7 +40,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  array.slice(0, array.lenght-1)
+  array.slice(array.lenght-1)
   return array
 }
 
