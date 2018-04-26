280b906584989b4634fe9ceb3595837765078528
Duc Dang <mrtrinh5293@gmail.com>
2018-04-26 04:50:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,17 +4,17 @@ function addElementToBeginningOfArray(array, element){
   return [element, ...array];
 }
 
-// function destructivelyAddElementToBeginningOfArray(array, element){
-//   array.unshift(element);
-//   return array
-// }
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return array
+}
 
-// function addElementToEndOfArray(array, element){
-//   // array.push(element);
-//   // return array
+function addElementToEndOfArray(array, element){
+  // array.push(element);
+  // return array
   
-//   return [...array, element];
-// }
+  return [...array, element];
+}
 
 // function destructivelyAddElementToEndOfArray(array, element){
 //   array.push(element);
