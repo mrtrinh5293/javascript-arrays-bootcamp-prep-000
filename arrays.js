df8069188429d740d3f82e8424846d63e2f0d5db
Duc Dang <mrtrinh5293@gmail.com>
2018-04-26 05:03:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -31,8 +31,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.shift();
-  return array;
+  return array.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
