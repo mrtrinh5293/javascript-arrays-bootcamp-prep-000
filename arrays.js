142b3cdc278b335fc01a13af62a367f7adb358da
Duc Dang <mrtrinh5293@gmail.com>
2018-04-26 04:51:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,8 +10,6 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 }
 
 function addElementToEndOfArray(array, element){
-  // array.push(element);
-  // return array
   
   return [...array, element];
 }
