cab8d7765c7a31847033b3e2b148bd97a03672b5
Duc Dang <mrtrinh5293@gmail.com>
2018-04-26 05:03:53 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,7 +34,10 @@ function removeElementFromBeginningOfArray(array){
   return array.slice(1);
 }
 
-
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
 
 function removeElementFromEndOfArray(array){
   return array.slice(0, array.lenght-1);
